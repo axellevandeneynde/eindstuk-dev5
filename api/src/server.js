92d453d694e2e6aa2754ff187a48fc52ae7e3401
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-02 12:03:32 +0100

    feat(server.js): db connection & init tables

--- api/src/server.js
+++ api/src/server.js
@@ -1,4 +1,18 @@
 const Hapi = require('@hapi/hapi');
+require('dotenv').config()
+const Helpers = require('./utils/helpers')
+const fs = require('fs');
+
+//--------- DB SETUP --------
+const pg = require('knex')({
+    client: 'pg',
+    version: '9.6',
+    searchPath: ['knex', 'public'],
+    connection: process.env.PG_CONNECTION_STRING ? process.env.PG_CONNECTION_STRING : 'postgres://example:example@localhost:5432/test'
+});
+initialiseTables();
+
+
 
 const init = async () => {
 
@@ -6,6 +20,7 @@ const init = async () => {
         port: 3000,
         host: '0.0.0.0'
     });
+
 
     server.route({
         method: 'GET',
@@ -27,3 +42,51 @@ process.on('unhandledRejection', (err) => {
 });
 
 init();
+
+
+//--------- INITIALISE TABLES --------
+// Checks if tables already exist in DB, 
+// otherwhise tables are created and filled when applicable
+
+async function initialiseTables() {
+    await pg.schema.hasTable("publications").then(async (exists) => {
+        if (!exists) {
+            await pg.schema
+                .createTable("publications", (table) => {
+                    table.increments();
+                    table.uuid("uuid");
+                    table.string("name");
+                    table.string("country_id");
+                    table.string("website_url");
+                })
+                .then(async () => {
+                    console.log("created table publications");
+                });
+        } else {
+            console.log('table publications exists')
+        }
+    });
+    await pg.schema.hasTable("countries").then(async (exists) => {
+        if (!exists) {
+            await pg.schema
+                .createTable("countries", (table) => {
+                    table.increments();
+                    table.uuid("uuid");
+                    table.string("name");
+                    table.string("code");
+                })
+                .then(async () => {
+                    console.log("created table countries");
+                    console.log("filling table countries...");
+                    const countries = Helpers.getCountries();
+                    countries.forEach(async (country) => {
+                        const uuid = Helpers.generateUUID();
+                        await pg.table("countries").insert({ uuid, name: country.name, code: country.code });
+                    });
+                    console.log("table countries filled");
+                })
+        } else {
+            console.log("table countries exists")
+        }
+    });
+}
