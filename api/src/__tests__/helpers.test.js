15d8ce1534deab4cb949f3fa9c72b4492537cc27
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-02 15:35:49 +0100

    test(helpers): test for checkIfValidSourceObject()

---
+++ api/src/__tests__/helpers.test.js
@@ -0,0 +1,20 @@
+const Helpers = require('../utils/helpers');
+
+describe('tests to check checkIfValidSourceObject()', () => {
+    test('source has to be an object', () => {
+        expect(Helpers.checkIfValidSourceObject([])).toBe(false)
+    })
+    test('source should have the right propreties', () => {
+        expect(Helpers.checkIfValidSourceObject({ hey: 'you', bla: 'good' }).toBe(false))
+    })
+    test('countryId source country id should exist'), () => {
+        expect(Helpers.checkIfValidSourceObject({
+            name: 'de morgen', country_id: 'LAP', website_url: 'https://demorgen.be'
+        }).toBe(false))
+    }
+    test('should return true when source is an object with right propreties and existing country id', () => {
+        expect(Helpers.checkIfValidSourceObject({
+            name: 'de morgen', country_id: 'BE', website_url: 'https://demorgen.be'
+        }).toBe(true))
+    })
+})
