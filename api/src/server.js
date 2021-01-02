2c17da2494e1f5377170bb36a166d3a1bde71d3a
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-02 16:17:03 +0100

    fix(server.js): remove console.log()

--- api/src/server.js
+++ api/src/server.js
@@ -39,7 +39,7 @@ const init = async () => {
     });
 
     await server.start();
-    console.log('Server running on %s', server.info.uri);
+    // console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
