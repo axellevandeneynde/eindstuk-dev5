5a69fba456b5276a809a66d6e5d01f8b06221fc7
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-02 16:11:47 +0100

    fix(api-endpoints.test): /add-new-source test

--- api/src/__tests__/integration/api-endpoints.test.js
+++ api/src/__tests__/integration/api-endpoints.test.js
@@ -2,10 +2,12 @@ const request = require('supertest');
 const server = require('../../server');
 
 describe('/add-new-source should accept a post request with a new source object', async () => {
-    const res = await request(server).post('/add-new-source').send({
-        name: 'De Morgen',
-        country_ID: 'BE',
-        Website_url: 'https://www.demorgen.be/'
-    })
+    const res = await request(server)
+        .post('/add-new-source')
+        .send({
+            name: 'De Morgen',
+            country_ID: 'BE',
+            website_url: 'https://www.demorgen.be/'
+        })
     expect(res.statusCode).toEqual(200)
 })
