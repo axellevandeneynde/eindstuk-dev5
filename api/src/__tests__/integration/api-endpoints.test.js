bb91780c73e0879b66c57b3f6583f7fab6a6cd44
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-02 16:33:33 +0100

    fix(api-endpoints.test): fix test error

--- api/src/__tests__/integration/api-endpoints.test.js
+++ api/src/__tests__/integration/api-endpoints.test.js
@@ -1,13 +1,16 @@
 const request = require('supertest');
 const server = require('../../server');
 
-describe('/add-new-source should accept a post request with a new source object', async () => {
-    const res = await request(server)
-        .post('/add-new-source')
-        .send({
-            name: 'De Morgen',
-            country_ID: 'BE',
-            website_url: 'https://www.demorgen.be/'
-        })
-    expect(res.statusCode).toEqual(200)
+describe('/add-new-source api endpoint', async () => {
+    test('should accept a post request with a new source object', async () => {
+        const res = await request(server)
+            .post('/add-new-source')
+            .send({
+                name: 'De Morgen',
+                country_ID: 'BE',
+                website_url: 'https://www.demorgen.be/'
+            });
+        expect(await res.statusCode).toEqual(200)
+    })
+
 })
