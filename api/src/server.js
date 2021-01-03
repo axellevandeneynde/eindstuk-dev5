040324d0aec198ccacc6420cc9752397b1dbc817
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-03 11:12:03 +0100

    feat(server): get sources with country id

--- api/src/server.js
+++ api/src/server.js
@@ -28,9 +28,10 @@ app.post('/add-new-source', (req, res) => {
     if (Helpers.checkIfValidSourceObject(newSource)) {
         newSource.uuid = Helpers.generateUUID();
         newSource.country_id = newSource.country_id.toUpperCase();
-        pg('publications')
-            .insert(newSource)
-        res.status(200).send({ message: `news source was added with uuid ${newSource.uuid}` })
+        pg.table('publications')
+            .insert(newSource).then(
+                res.status(200).send({ message: `news source was added with uuid ${newSource.uuid}` })
+            )
     } else {
         res.status(400).send({ message: 'invalid news source object' })
     }
@@ -38,28 +39,28 @@ app.post('/add-new-source', (req, res) => {
 })
 
 
+//-- get news sources --
+app.get('/sources/:country_id', (req, res) => {
+    if (Helpers.checkIfValidCountryId(req.params.country_id)) {
+        pg.from('publications')
+            .select(['name', 'website_url', 'country_id'])
+            .where({ 'country_id': req.params.country_id.toUpperCase() })
+            .then(data => {
+                res.status(200).send({ publications: data })
+            })
+
+    } else {
+        res.status(400).send({ message: 'invalid country_id' })
+    }
+})
+
+
+
 //--------- INITIALISE TABLES --------
 // Checks if tables already exist in DB, 
 // otherwhise tables are created and filled when applicable
 
 async function initialiseTables() {
-    await pg.schema.hasTable("publications").then(async (exists) => {
-        if (!exists) {
-            await pg.schema
-                .createTable("publications", (table) => {
-                    table.increments();
-                    table.uuid("uuid");
-                    table.string("name");
-                    table.string("country_id");
-                    table.string("website_url");
-                })
-                .then(async () => {
-                    console.log("created table publications");
-                });
-        } else {
-            console.log('table publications exists')
-        }
-    });
     await pg.schema.hasTable("countries").then(async (exists) => {
         if (!exists) {
             await pg.schema
@@ -81,6 +82,23 @@ async function initialiseTables() {
                 })
         } else {
             console.log("table countries exists")
+        }
+    });
+    await pg.schema.hasTable("publications").then(async (exists) => {
+        if (!exists) {
+            await pg.schema
+                .createTable("publications", (table) => {
+                    table.increments();
+                    table.uuid("uuid");
+                    table.string("name");
+                    table.string("website_url");
+                    table.string("country_id").references("code").inTable("countries");
+                })
+                .then(async () => {
+                    console.log("created table publications");
+                });
+        } else {
+            console.log('table publications exists')
         }
     });
 }
