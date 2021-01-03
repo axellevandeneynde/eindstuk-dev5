ae1365cbf638b02322f576e93f49df03b4aaee1f
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-03 10:13:55 +0100

    test(api-endpoints): new test /sources/:country_id

--- api/src/__tests__/integration/api-endpoints.test.js
+++ api/src/__tests__/integration/api-endpoints.test.js
@@ -23,3 +23,16 @@ describe('/add-new-source api endpoint', () => {
         expect(await res.statusCode).toEqual(400)
     });
 })
+
+describe('/sources/:country_id endpiont get sources from country', () => {
+    test('should not accept non existing country ID', async () => {
+        const res = await request(server)
+            .get('/sources/LAP')
+        expect(await res.statusCode).toEqual(400)
+    })
+    test('should respond data if existing country ID', async () => {
+        const res = await request(server)
+            .get('/sources/BE')
+        expect(await res.body).toBeDefined()
+    })
+})
