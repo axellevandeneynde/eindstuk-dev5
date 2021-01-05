45cd2866bc50b73b82b7627c4338f0f161dae228
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-05 16:36:55 +0100

    test(end-to-end): base

---
+++ api/src/__tests__/end-to-end.test.js
@@ -0,0 +1,40 @@
+const supertest = require('supertest');
+const server = require('../server.js');
+
+const request = supertest(server);
+
+describe('end-to-end CRUD publication', () => {
+    test('create publication', async () => {
+        const res = await request
+            .post('/add-new-source')
+            .send({
+                name: 'de morgen',
+                country_id: 'BE',
+                website_url: 'https://demorgen.be'
+            });
+        expect(await res.statusCode).toEqual(200)
+    })
+
+    test('read publication from the country', async () => {
+        const res = await request
+            .get('/sources/BE')
+        expect(await res.statusCode).toEqual(200)
+    })
+
+    test('update publication with uuid', async () => {
+        const res = await request
+            .post(`/update-publication/78204893-2c9e-4fd8-a1bb-8786034ddb2a`).send({
+                country_id: 'NL'
+            })
+        expect(await res.statusCode).toEqual(200)
+    })
+
+    test('delete publication with name', async () => {
+        const res = await request
+            .post('/delete-publication')
+            .send({
+                name: 'de morgen',
+            });
+        expect(await res.statusCode).toEqual(200)
+    })
+})
