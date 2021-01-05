405742da3eb696b7b8ebc67bdc18107e855c6aa3
Axelle.vde <axelle.vanden.eynde@student.ehb.be>
2021-01-05 15:38:43 +0100

    test: extra integration tests /update-publication

--- api/src/__tests__/integration/api-endpoints.test.js
+++ api/src/__tests__/integration/api-endpoints.test.js
@@ -60,13 +60,36 @@ describe('/delete-publication with name', () => {
 describe('/update-publication with uuid', () => {
     test('should respond to valid request positively', async () => {
         const res = await request
-            .post('/update-publication:515b69c0-473b-11eb-8799-adb231290ef6')
+            .post('/update-publication:515b69c0-473b-11eb-8799-adb231290ef6').send({
+                name: 'the new york times'
+            })
         expect(await res.body).toHaveProperty('message')
         expect(await res.statusCode).toEqual(200)
     })
-    test('should respond to request without uuid negativelt', async () => {
+    test('should respond to request without uuid negativly', async () => {
         const res = await request
-            .post('/update-publication:something')
+            .post('/update-publication:something').send({
+                name: 'the new york times'
+            })
+        expect(await res.statusCode).toEqual(400)
+    })
+    test('should respond to request with wrong proprety names negatively', async () => {
+        const res = await request
+            .post('/update-publication:something').send({
+                post: 'the new york times'
+            })
+        expect(await res.statusCode).toEqual(400)
+    })
+    test('should respond to request with wrong proprety names negatively', async () => {
+        const res = await request
+            .post('/update-publication:515b69c0-473b-11eb-8799-adb231290ef6').send({
+                post: 'the new york times'
+            })
+        expect(await res.statusCode).toEqual(400)
+    })
+    test('should respond to request with empty body negatively', async () => {
+        const res = await request
+            .post('/update-publication:515b69c0-473b-11eb-8799-adb231290ef6').send({})
         expect(await res.statusCode).toEqual(400)
     })
 })
